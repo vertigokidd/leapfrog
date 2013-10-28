9a789aaf4f07c67ad4bbc68875d97d4c0f97953d
Michael Rhee <mjrhee@gmail.com>
2013-10-28 12:15:39 -0500

    Rename rb files and class

--- spec/leapfrogger_spec.rb
+++ spec/leapfrogger_spec.rb
@@ -1,39 +1,39 @@
 require 'spec_helper'
 require 'fakeweb'
 
-describe Frogger do
+describe Leapfrogger do
 
   before :each do
-    @frogger = Frogger.new(50000, 60201, 35)
+    @leapfrogger = Leapfrogger.new(50000, 60201, 35)
   end
 
   describe "#new" do
-    it "takes three parameters and returns a Frogger object" do
-      @frogger.should be_an_instance_of Frogger
+    it "takes three parameters and returns a Leapfrogger object" do
+      @leapfrogger.should be_an_instance_of Leapfrogger
     end
   end
 
   describe "#income" do
     it "returns the correct income" do
-      @frogger.income.should eql(50000)
+      @leapfrogger.income.should eql(50000)
     end
   end
 
   describe "#zip_code" do
     it "returns the correct zip_code" do
-      @frogger.zip_code.should eql(60201)
+      @leapfrogger.zip_code.should eql(60201)
     end
   end
 
   describe "#age" do
     it "returns the correct age" do
-      @frogger.age.should eql(35)
+      @leapfrogger.age.should eql(35)
     end
   end
 
   describe "#create_request" do
     it "creates a url string with income, zip_code and age as params" do
-      @frogger.create_request.should eq("http://internal.leapfrogonline.com/customer_scoring?income=50000&zipcode=60201&age=35")
+      @leapfrogger.create_request.should eq("http://internal.leapfrogonline.com/customer_scoring?income=50000&zipcode=60201&age=35")
     end
   end
 
@@ -42,16 +42,16 @@ describe Frogger do
     it "makes an http get request to correct url" do
       FakeWeb.register_uri(:get, "http://internal.leapfrogonline.com/customer_scoring?income=50000&zipcode=60201&age=35", 
                            :body => "{ 'propensity': 0.26532, 'ranking': 'C' }")
-      @frogger.fetch.should include("propensity")
+      @leapfrogger.fetch.should include("propensity")
     end
 
     it "returns an error if response is not OK" do
       FakeWeb.register_uri(:get, "http://internal.leapfrogonline.com/customer_scoring?income=50000&zipcode=60201&age=35", 
                            :body => "{ 'propensity': 0.26532, 'ranking': 'C' }",
                            :status => ["404", "Not Found"])
-      @frogger.fetch.should include("error")
+      @leapfrogger.fetch.should include("error")
     end
-    
+
   end
   
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,1 @@
-require_relative '../api.rb'
+require_relative '../leapfrogger.rb'
