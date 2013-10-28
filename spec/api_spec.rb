185a270ebec41f94d6f3248fff309286ee1ea4f9
Michael Rhee <mjrhee@gmail.com>
2013-10-28 11:21:29 -0500

    Add tests for Frogger attrs

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -11,5 +11,23 @@ describe Frogger do
       @frogger.should be_an_instance_of Frogger
     end
   end
+
+  describe "#income" do
+    it "returns the correct income" do
+      @book.income.should eql(50000)
+    end
+  end
+
+  describe "#zip_code" do
+    it "returns the correct zip_code" do
+      @book.zip_code.should eql(60201)
+    end
+  end
+
+  describe "#age" do
+    it "returns the correct age" do
+      @book.age.should eql(35)
+    end
+  end
   
 end
